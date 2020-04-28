99ebacf456b3500d586a4d0cd1fc3ec0a3eb218e
Sebasti14 <51194064+Sebasti14@users.noreply.github.com>
2020-04-28 14:12:16 +0530

    Update ec2_key_pair.tf

--- ec2_key_pair.tf
+++ ec2_key_pair.tf
@@ -1,6 +1,8 @@
 ## Create the key pair to access the EC2 instance ##
 
-resource "aws_key_pair" "eb-ec2-key-pair" {
-  key_name_prefix = "EB-${var.app_name}-key"
-  public_key = file("${path.module}/pub_key")
-}
+## Uncomment below lines only if we need to access the Ec2 instance ##
+
+#resource "aws_key_pair" "eb-ec2-key-pair" {
+#  key_name_prefix = "EB-${var.app_name}-key"
+#  public_key = file("${path.module}/pub_key")
+#}
