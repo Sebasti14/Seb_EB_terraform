2924faa5a20015021aaebe6aac8592a42ffbfbe0
Sebasti14 <51194064+Sebasti14@users.noreply.github.com>
2020-04-29 13:51:25 +0530

    Update s3_bucket_app.tf

--- s3_bucket_app.tf
+++ s3_bucket_app.tf
@@ -1,21 +1,22 @@
 # Create S3 bucket to store the application deployment files ##
 
 resource "aws_s3_bucket" "eb-bucket" {
-  #bucket = "eb-app-bucket"
-  bucket_prefix = "elasticbeanstalk-${var.aws_region}-${var.app_name}"
-  region = var.aws_region
+  bucket = "elasticbeanstalk-${var.aws_region}-${var.aws_account_id}"
+#  bucket_prefix = "elasticbeanstalk-${var.aws_region}-${var.app_name}"
+#  region = var.aws_region
   acl    = "private"
   tags = {
-    Name  = "eb-app-bucket"
+    Name  = "eb-bucket"
   }
   versioning {
     enabled = true
   }
-
 }
+
 resource "aws_s3_bucket_object" "eb-bucket-object" {
   bucket = aws_s3_bucket.eb-bucket.id
-  key    = "sample.war"
+#  bucket = "elasticbeanstalk-${var.aws_region}-${var.aws_account_id}"
+  key    = "EB-${var.app_name}-appcode/sample.war"
   source = "./sample.war"
-  #kms_key_id = var.kms_key_id
 }
+
